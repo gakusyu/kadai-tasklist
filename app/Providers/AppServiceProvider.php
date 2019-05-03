b335abe34050fd930582693df68bc3a1b100047c
gakusyu <gakusyu2019@gmail.com>
2019-05-03 05:24:15 +0000

    update_composer_library

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-    }
+        \Schema::defaultStringLength(191);
+        }
 
     /**
      * Register any application services.
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        \Schema::defaultStringLength(191);
+        //
     }
 }
